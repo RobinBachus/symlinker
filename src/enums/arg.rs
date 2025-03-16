d194db217dc1a580e641b45020139cf6cbf7453d
RobinBachus <robinbachus@gmail.com>
2025-03-16 01:04:14 +0100

    Small refactor to arg handling

--- src/enums/arg.rs
+++ src/enums/arg.rs
@@ -8,7 +8,7 @@ pub enum Arg {
 
 impl Arg {
     pub fn from_str(s: &str) -> Arg {
-        match s {
+        match s.trim().to_lowercase().as_str() {
             "a" | "add" => Arg::Add,
             "r" | "remove" => Arg::Remove,
             "l" | "list" => Arg::List,
